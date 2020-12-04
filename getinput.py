4f16f365d9230f81fd4f5ecc8e58b04c00102a78
James Wilcock <jameswilcock8@gmail.com>
2020-12-03 19:26:54 -0800

    fixed unecessary filemaking

--- getinput.py
+++ getinput.py
@@ -4,6 +4,8 @@ import sys
 with open('secrets.txt', 'r') as f:
     secret = f.read().strip()
 #python3 getinput.py {day}
+#todo create directorys
+#and basic python file if not present
 x = sys.argv[1]
 cookies = {
         'session': secret
@@ -13,5 +15,8 @@ headers = {
         }
 
 response = requests.get(f'http://adventofcode.com/2020/day/{x}/input', headers=headers, cookies=cookies)
+print(str(response.content[0:10],'utf-8'))
 with open(f'day{x}/input.txt', 'wb') as f:
-    f.write(response.content)
+    if str(response.content[0:3],'utf-8') != "Ple":
+        f.write(response.content)
+        print("wrote to file")
