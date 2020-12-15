e7fc119b1e008b7767cb26034982b463ff515ed3
2chainzbear <jameswilcock8@gmail.com>
2020-12-14 23:04:48 -0800

    added day15 solution

---
+++ day15/main.py
@@ -0,0 +1,37 @@
+import fileinput
+from collections import Counter
+from collections import defaultdict
+import re
+
+inputlist = []
+for line in fileinput.input():
+    inputlist.append(line.strip())
+
+print(inputlist[0])
+splitlist = inputlist[0].split(',')
+splitlist = [int(x) for x in splitlist]
+print(splitlist)
+whensaid = []
+for x in range(len(splitlist)):
+    whensaid.append([])
+print(whensaid)
+def p1(splitlist):
+    num = 0
+    #have to keep list of all nums and when they were said, perhaps dict
+    numdict = defaultdict(list)
+    mylen = len(splitlist)
+    for cnt in range(30000000):
+        if cnt < mylen:
+            num = splitlist[cnt]
+        else:
+            if num in numdict:
+                if len(numdict[num]) == 1:
+                    num = 0
+                elif len(numdict[num]) > 1:
+                    num = numdict[num][-1] - numdict[num][-2]
+        numdict[num].append(cnt)
+    return num
+#print("test 0,3,6",p1([0,3,6]))
+#import timeit
+#print(timeit.timeit(lambda: p1([0,3,6]),number = 1))
+print("p1: ", p1(splitlist))
