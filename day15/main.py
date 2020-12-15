e649703c19a8495e5acf478416be298123b4558a
2chainzbear <jameswilcock8@gmail.com>
2020-12-15 12:28:43 -0800

    changed solution to much faster deque approach

--- day15/main.py
+++ day15/main.py
@@ -1,6 +1,8 @@
 import fileinput
 from collections import Counter
 from collections import defaultdict
+from collections import deque
+from functools import partial
 import re
 
 inputlist = []
@@ -11,10 +13,13 @@ print(inputlist[0])
 splitlist = inputlist[0].split(',')
 splitlist = [int(x) for x in splitlist]
 print(splitlist)
+
+
 def p1(splitlist):
     num = 0
-    #have to keep list of all nums and when they were said, perhaps dict
-    numdict = defaultdict(list)
+    # have to keep list of all nums and when they were said, perhaps dict
+    mydeque = partial(deque, maxlen=2)
+    numdict = defaultdict(mydeque)
     mylen = len(splitlist)
     for cnt in range(30000000):
         if cnt < mylen:
@@ -27,7 +32,9 @@ def p1(splitlist):
                     num = numdict[num][-1] - numdict[num][-2]
         numdict[num].append(cnt)
     return num
-#print("test 0,3,6",p1([0,3,6]))
-#import timeit
-#print(timeit.timeit(lambda: p1([0,3,6]),number = 1))
+
+
+print("test 0,3,6:", p1([0, 3, 6]))
+# import timeit
+# print(timeit.timeit(lambda: p1([0,3,6]),number = 1))
 print("p1: ", p1(splitlist))
