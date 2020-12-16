a6575d3e68ed86a83e011c2a29c1e22dcb529d10
2chainzbear <jameswilcock8@gmail.com>
2020-12-15 22:10:23 -0800

    p1 solved

--- day16/main.py
+++ day16/main.py
@@ -1,5 +1,7 @@
 # Advent of Code day 16
 import re
+def inrange(lohi,num):
+    return lohi[0] <= num <= lohi[1]
 inputlist = []
 with open('input.txt','r') as f:
     inputlist = f.readlines()
@@ -17,5 +19,19 @@ for x in inputlist[0:20]:
         ints = [int(x) for x in ints]
         lohis.append((ints[0],ints[1]))
         lohis.append((ints[2],ints[3]))
+nearby = []
 for x in inputlist[25:]:
-    print(x)
+    nearby.append(x.split(','))
+nearby = [int(item) for sublist in nearby for item in sublist]
+invalidlist = []
+for x in nearby:
+    valid = 0
+    for lohi in lohis:
+        if inrange(lohi,x):
+            valid += 1
+    if valid == 0:
+        invalidlist.append(x)
+print("p1",sum(invalidlist))
+
+for x in invalidlist:
+    nearby.remove(x)
