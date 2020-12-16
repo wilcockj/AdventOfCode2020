18c1a07e2104c5d9dfc098f9a1d94dee588e28a3
2chainzbear <jameswilcock8@gmail.com>
2020-12-15 22:18:17 -0800

    p2 almost finished

--- day16/main.py
+++ day16/main.py
@@ -11,14 +11,17 @@ inputlist = [x.strip('\n') for x in inputlist]
 getints = re.compile('\d+')
 gettype = re.compile('(.*):')
 lohis = []
+lohidict = {}
 for x in inputlist[0:20]:
     
     ints = getints.findall(x)
     numbertype = gettype.match(x)
+    numbertype = numbertype[0]
     if ints:
         ints = [int(x) for x in ints]
         lohis.append((ints[0],ints[1]))
         lohis.append((ints[2],ints[3]))
+        lohidict[numbertype] = [(ints[0],ints[1]),(ints[2],ints[3])]
 nearby = []
 for x in inputlist[25:]:
     nearby.append(x.split(','))
@@ -32,6 +35,14 @@ for x in nearby:
     if valid == 0:
         invalidlist.append(x)
 print("p1",sum(invalidlist))
-
+myticket = [int(x) for x in inputlist[22].split(',')]
+print(myticket)
+ticketdict = {}
 for x in invalidlist:
     nearby.remove(x)
+for count, ticketnum in enumerate(nearby):
+    for key,value in lohidict.items():
+        for lohi in value:
+            if inrange(lohi,ticketnum) and key not in ticketdict and count not in ticketdict.values():
+                ticketdict[key] = count
+print(ticketdict)
